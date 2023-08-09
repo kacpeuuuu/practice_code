16af8dcef2a281d80e9e048f7c586becd4ddf0e7
unknown <bielawa.kacper6@gmail.com>
2023-08-09 13:50:09 +0200

    first commit

---
+++ PONG/pong.py
@@ -0,0 +1,12 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800,height=600)
+window.tracer(0)
+
+
+while True:
+    window.update()
+    
